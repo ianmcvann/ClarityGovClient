5a5538a59b3f86b085689060b769df5c7ea38ec7
Ian McVann <ian.mcvann@gmail.com>
2023-07-25 09:47:13 -0500

    remove unneccesary tests

--- tests/test_api_client.py
+++
@@ -1,7 +0,0 @@
-from claritygov.api_client import ClarityAPIClient
-import pytest
-
-def test_get():
-    client = ClarityAPIClient()
-    response = client.get('/md/house/members')
-    assert response.status_code == 200
